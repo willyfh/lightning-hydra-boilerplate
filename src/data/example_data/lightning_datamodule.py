fb11caa1cceba9a723c59d797596f4bf31619f66
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2025-04-05 17:10:17 +0900

    Support run test after training

--- src/data/example_data/lightning_datamodule.py
+++ src/data/example_data/lightning_datamodule.py
@@ -9,11 +9,19 @@ class ExampleDataModule(L.LightningDataModule):
         self.num_workers = num_workers
 
     def setup(self, stage=None):
-        full_dataset = ExampleTorchDataset(train=True)
-        self.train_dataset, self.val_dataset = random_split(full_dataset, [55000, 5000])
+        full_train_dataset = ExampleTorchDataset(train=True)
+        full_test_dataset = ExampleTorchDataset(train=False)
+
+        # Split the training dataset into training and validation datasets
+        self.train_dataset, self.val_dataset = random_split(full_train_dataset, [55000, 5000])
+
+        self.test_dataset = full_test_dataset
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
